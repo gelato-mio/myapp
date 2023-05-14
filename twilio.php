152e89fd175f04760e05107daf39c9c9546da923
Gelato <cyberdefence@osint.local>
2023-05-14 21:03:16 +0300

    new

--- twilio.php
+++ twilio.php
@@ -1,5 +1,5 @@
 <?php
 $to = "ch.cyber@proton.me";
 $subject = "Text Message from {$_REQUEST[‘From’]} to {$_REQUEST[‘To’]}"; $message = "{$_REQUEST[‘Body’]}";
-$headers = "From: twilio@sip_messaging.com";
+$headers = "From: ch.cyber@proton.me";
 mail($to, $subject, $message, $headers);
