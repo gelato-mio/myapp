5a4293bba0b8ac472ccc8882c97befe22bb62e73
Gelato <cyberdefence@osint.local>
2023-05-15 21:11:26 +0300

    test

--- twilio.php
+++ twilio.php
@@ -1,4 +1,16 @@
-<?php
+<?php 
+/** 
+* This section ensures that Twilio gets a response. 
+*/ 
+header('Content-type: text/xml');
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+echo '<Response></Response>'; //Place the desired response (if any) here
+
+/** 
+* This section actually sends the email. 
+*/ 
+
+
 $to = "ch.cyber@proton.me";
 $subject = "Text Message from {$_REQUEST[‘From’]} to {$_REQUEST[‘To’]}"; 
 $message = "{$_REQUEST[‘Body’]}";
